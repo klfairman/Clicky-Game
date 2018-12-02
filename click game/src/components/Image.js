c579bfb3d9e5c9292a210943e2368ede7c29a37a
Kristen Fairman <fairman.kristen@gmail.com>
2018-12-02 16:59:26 -0600

    pewpew

--- click game/src/components/Image.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Image = (props) => (
-
-    <img className={"img-thumbnail"} src={require(`../img/${props.imgSrc}.gif`)} alt={props.imgSrc}/>
-
-);
-
-export default Image;
