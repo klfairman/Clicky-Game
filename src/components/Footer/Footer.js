00076b8b3f081c50e062002e7154e06328cb7298
Kristen Fairman <fairman.kristen@gmail.com>
2018-12-04 10:57:45 -0600

    updates

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => <div className="footer text-center">Kristen Fairman</div>;
+
+export default Footer;
---
+++ src/images.js
@@ -0,0 +1,29 @@
+import atat from './images/AT-AT.png';
+import bb8 from './images/bb8.jpeg';
+import c3p0 from './images/C3P0.png';
+import chewy from './images/chewy.png';
+import darthmaul from './images/darthmaul.jpeg';
+import deathstar from './images/deathstar.png';
+import ewok from './images/ewok.png';
+import luke from './images/luke.png';
+import millenfalc from './images/millenfalc.png';
+import r2d2 from './images/R2D2.png';
+import stormtrooper from './images/stormtrooper.png';
+import yoda from './images/yoda.png';
+
+const images = [
+  atat,
+  bb8,
+  c3p0,
+  chewy,
+  darthmaul,
+  deathstar,
+  ewok,
+  luke,
+  millenfalc,
+  r2d2,
+  stormtrooper,
+  yoda
+];
+
+export default images;
